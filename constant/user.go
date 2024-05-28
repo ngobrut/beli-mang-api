963497c8110b722cc2ebdf95bb2d72db6bf86028
fatih.dq19 <samgong23@gmail.com>
2024-05-29 01:27:10 +0700

    fix: change dir constant gender

--- constant/user.go
+++ constant/user.go
@@ -6,3 +6,20 @@ const (
 	AdminRole Role = "admin"
 	UserRole  Role = "user"
 )
+
+type Gender string
+
+const (
+	Male   Gender = "male"
+	Female Gender = "female"
+)
+
+var ValidGender = map[string]bool{
+	string(Male):   true,
+	string(Female): true,
+}
+
+var Genders = []string{
+	string(Male),
+	string(Female),
+}
